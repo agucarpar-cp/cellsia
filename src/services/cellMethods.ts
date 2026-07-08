92abed5c3bfd468efbe40b5f18c23853d24f376e
agucarpar-cp <agucarpar@gmail.com>
2026-07-08 07:15:37 +0200

    rename functions

--- src/services/cellMethods.ts
+++ src/services/cellMethods.ts
@@ -1,16 +1,18 @@
 import { api } from "./api";
-import { dummyAvailableMethodsList } from "../utils/libs/dummyLib";
+import { dummyAvailableSampleList } from "../utils/libs/dummyLib";
 
-export async function getOneCellMethod() {
-  const response = await api.get("http://localhost:3000/cell-methods");
+export async function getOneCellSample(sampleName: string) {
+  const response = await api.get(
+    `/mini-nucleiq-code-challenge/samples/${sampleName}.json`,
+  );
   return response;
 }
 
 // Simulates an API call that returns the list of available samples.In production, this would fetch from a real backend endpoint.
 //
-export async function dummyGetAllCellMethods(): Promise<string[]> {
+export async function dummyGetAllCellSamples(): Promise<string[]> {
   await new Promise((resolve) => setTimeout(resolve, 300));
 
   // Return the dummy data as if it came from the API
-  return dummyAvailableMethodsList;
+  return dummyAvailableSampleList;
 }
--- src/utils/libs/dummyLib.ts
+++ src/utils/libs/dummyLib.ts
@@ -1,6 +1,6 @@
 // We are cheating here. This file is a dummy file where we define a list of available methods. But, actually, the method list must be fetched from the backend, in a SSR component, storage in Context/redux/Zustand/pinia/.. and import there where it would be used. This is a temporary solution to avoid having to implement the full backend integration for now. We will use this dummyAvailableMethodsList to simulate the backend response.
 
-export const dummyAvailableMethodsList = [
+export const dummyAvailableSampleList = [
   "sample-a",
   "sample-b",
   "sample-c",
