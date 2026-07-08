d9e754ff3910ee622484cd7524fa3e30073df92b
agucarpar-cp <agucarpar@gmail.com>
2026-07-08 05:30:40 +0200

    improve methods

--- src/utils/detectionMethos/index.ts
+++ src/utils/detectionMethos/index.ts
@@ -1,24 +1,24 @@
 export function evenZeroesMethod(sample: number[]): boolean {
-  if (!sample || sample.length === 0) return false;
+  const sampleLength = sample.length;
+  if (!sample || sampleLength === 0 || sampleLength < 2) return false;
 
   const isEvenIndex = (index: number) => index % 2 === 0;
   const isZero = (value: number) => value === 0;
   const isPositiveCell = (value: number, index: number) =>
     isEvenIndex(index) && isZero(value);
 
-  const totalCells = sample.length;
   const positiveCells = sample.filter(isPositiveCell).length;
   const percentageRequired = 0.3;
 
-  const result = positiveCells > totalCells * percentageRequired;
-
-  return result;
+  return positiveCells > sampleLength * percentageRequired;
 }
 
 export function contiguousOnesMethod(sample: number[]): boolean {
-  if (!sample || sample.length === 0) return false;
+  const sampleLength = sample.length;
+
+  if (!sample || sampleLength === 0 || sampleLength < 2) return false;
   let countContiguousOnes = 0;
-  for (let i = 0; i < sample.length - 1; i++) {
+  for (let i = 0; i < sampleLength - 1; i++) {
     if (sample[i] === 1 && sample[i + 1] === 1) {
       countContiguousOnes++;
     }
@@ -26,19 +26,21 @@ export function contiguousOnesMethod(sample: number[]): boolean {
 
   const percentageRequired = 0.2;
 
-  return countContiguousOnes > sample.length * percentageRequired;
+  return countContiguousOnes > sampleLength * percentageRequired;
 }
 
 export function surroundedOnesByZeroes(sample: number[]): boolean {
-  if (!sample || sample.length === 0) return false;
+  const sampleLength = sample.length;
+
+  if (!sample || sampleLength === 0 || sampleLength < 3) return false;
   let surroundedOnes = 0;
-  for (let i = 1; i < sample.length - 1; i++) {
-    if (sample[i] === 1 && sample[i + 1] === 0 && sample[i - 1] === 0) {
+  for (let i = 1; i < sampleLength - 1; i++) {
+    if (sample[i - 1] === 0 && sample[i] === 1 && sample[i + 1] === 0) {
       surroundedOnes++;
     }
   }
 
   const percentageRequired = 0.1;
 
-  return surroundedOnes > sample.length * percentageRequired;
+  return surroundedOnes > sampleLength * percentageRequired;
 }
