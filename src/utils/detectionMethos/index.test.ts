9abe7bc75288d251c56200c28922b3e82628ce19
agucarpar-cp <agucarpar@gmail.com>
2026-07-08 04:55:50 +0200

    add dummy data for testing

--- src/utils/detectionMethos/index.test.ts
+++ src/utils/detectionMethos/index.test.ts
@@ -1,10 +1,11 @@
 /// <reference types="jest" />
 
 import { evenZeroesMethod } from ".";
+import { evenZeroesMethodFalse, evenZeroesMethodTrue } from "../dummyLib";
 
 describe("math utilities", () => {
   test("evenZeroesMethod returns true for arrays with more than 30% even-indexed zeroes", () => {
-    expect(evenZeroesMethod([0, 1, 0, 2, 0, 3])).toBe(true);
-    expect(evenZeroesMethod([1, 2, 3, 4, 5, 6])).toBe(false);
+    expect(evenZeroesMethod(evenZeroesMethodTrue)).toBe(true);
+    expect(evenZeroesMethod(evenZeroesMethodFalse)).toBe(false);
   });
 });
